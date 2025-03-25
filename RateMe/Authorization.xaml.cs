d6c664184f28565a8a29f25f58146a9c72986e60
M0s1ck from laptop <senyamoskin@gmail.com>
2025-03-25 12:01:12 +0300

    Initialize authorization window

---
+++ RateMe/Authorization.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RateMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WelcomeBackButton.IsChecked != null && WelcomeBackButton.IsChecked == true)
+            {
+                MessageBox.Show("Welcome back!");
+            }
+            else if (WelcomeButton.IsChecked != null && WelcomeButton.IsChecked == true)
+            {
+                MessageBox.Show("Welcome!");
+            }
+        }
+    }
+}
